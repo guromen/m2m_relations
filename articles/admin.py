a776b84b4d02dc0fcec4503cb482187a0f60389c
Roman Guguzin <r-0men@yandex.ru>
2024-11-13 16:25:52 +0300

    Initial commit

---
+++ articles/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, Tag
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+               count += 1
+            if count != 1:
+             raise ValidationError('Должен быть один главный тэг')
+        return super().clean()
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
